63863974be394c75eb41ee0493f55a336136d919
pyKitchen <info@pykitchen.net>
2014-01-06 08:55:49 +0100

    testing geos

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pyKitchen/newmath"
+        "github.com/paulsmith/gogeos/geos"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    line, _ := geos.FromWKT("LINESTRING (0 0, 10 10, 20 20)")
+    buf, _ := line.Buffer(2.5)
+    fmt.Printf("Hello, world.  Sqrt(2) = %v%v\n", newmath.Sqrt(2), buf)
 }
